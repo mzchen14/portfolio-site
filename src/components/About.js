2606eb2967d8b1320945e726828999d506de29dd
Ming Chen <mzchen14@gmail.com>
2020-07-09 22:38:31 -0400

    Added Social Links and 404 page

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
     }
     `)
     return (
-    <div id="container" className='about-section'>
+    <div id="about" className='about-section'>
          <div className="profile-image">
             <h1 className="about-me">About Me</h1>
             <Img
@@ -34,13 +34,12 @@ const About = () => {
         <div className='text'>
             <h3>My journey...</h3>
             <p>
-            Hello! I am a software engineer based in New York City. 
-            Just a year ago, I was working as an elementary school teacher as well as a freelance digital illustrator. 
-            Wanting to combine my love for creativity and problem solving, 
-            I began to explore and grow my passion for software development and ended up attending the Grace Hopper program at Fullstack Academy.
-            To this day, I believe it was one of the best decisions I've made. I was able to challenge myself in ways I never imagined and came out even more immersed in programming.
-            Now as a developer, I hope to use my skills to contribute to applications that benefit the lives of others. :)
-            </p>
+            Hello! I am a software engineer based in New York City. I graduated from SUNY Stony Brook with a bachelors in English Literature.
+            Just a year ago, I was working as an elementary school teacher as well as a freelance digital illustrator. </p>
+            <p>Wanting to combine my love for creativity and problem solving, 
+            I began to explore and grow my passion for software development and ended up attending the Grace Hopper program at Fullstack Academy. </p> 
+            <p>To this day, I believe it was one of the best decisions I've made. I was able to challenge myself in ways I never imagined and came out even more immersed in programming.</p>
+            <p>Now as a developer, I hope to use my skills to contribute to applications that benefit the lives of others. :)</p>
             <h3>When I'm not coding...</h3>
             <p>
             In my free time, I enjoy traveling, digital painting, listening to true crime podcasts, playing video games, reading novels, and trying new foods!
