8dcd564db45aaa331329943cedc671b288c14309
Ming Chen <mzchen14@gmail.com>
2020-07-11 13:25:28 -0400

    Edited Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import "./Contact.css"
 
 export default () => (
-    <div id="contact">
-        <form 
-        name="contact" 
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        >
-            <input name="name" placeholder="Your Name" type="text"/>
-            <button>Send</button>
+    <div id="contact-form">
+        <form className="form" name="contact" netlify>
+            <div className="input">
+                <label htmlFor="name">Name</label>
+                <input name="name" placeholder="Your Name" type="text"/>
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input name="email" placeholder="Your Email" type="email"/>
+            </div>
+            <div>
+                <label htmlFor="message">Message</label>
+                <textarea name="message"/>
+            </div>
+            <button type="submit">Send</button>
         </form>
     </div>
 )
