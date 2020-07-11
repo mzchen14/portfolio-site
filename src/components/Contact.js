2cfd0bf019eb23deb901a4b855ca2ecf41496993
Ming Chen <mzchen14@gmail.com>
2020-07-11 13:11:37 -0400

    Add form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default () => (
     <div id="contact">
-        Contact Me
+        <form 
+        name="contact" 
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        >
+            <input name="name" placeholder="Your Name" type="text"/>
+            <button>Send</button>
+        </form>
     </div>
 )
