5bc8e01dfef5348529a2c5edab4a32dac3ebfc7c
Ming Chen <mzchen14@gmail.com>
2020-07-11 13:58:39 -0400

    Changed layout of form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,12 @@ import "./Contact.css"
 
 export default () => (
         <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-            <div className="input">
                 <label htmlFor="name">Name</label>
                 <input name="name" placeholder="Your Name" type="text"/>
-            </div>
-            <div>
                 <label htmlFor="email">Email</label>
                 <input name="email" placeholder="Your Email" type="email"/>
-            </div>
-            <div>
                 <label htmlFor="message">Message</label>
                 <textarea name="message"/>
-            </div>
             <button type="submit">Send</button>
         </form>
 )
