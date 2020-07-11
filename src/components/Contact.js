0542d11efc32eeb3a89c33d483f8a9890c7e3a89
Ming Chen <mzchen14@gmail.com>
2020-07-11 13:30:24 -0400

    Changes to Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import "./Contact.css"
 
 export default () => (
     <div id="contact-form">
-        <form className="form" name="contact" netlify>
+        <form className="form" name="contact" data-netlify="true" data-netlify-honeyput="bot-field">
             <div className="input">
                 <label htmlFor="name">Name</label>
                 <input name="name" placeholder="Your Name" type="text"/>
