0ea4e40dc206c7ad8e25847444a2155c436f9436
Ming Chen <mzchen14@gmail.com>
2020-07-11 14:03:11 -0400

    Reverting form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import "./Contact.css"
 
 export default () => (
-        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                <label htmlFor="name">Name</label>
-                <input name="name" placeholder="Your Name" type="text"/>
-                <label htmlFor="email">Email</label>
-                <input name="email" placeholder="Your Email" type="email"/>
-                <label htmlFor="message">Message</label>
-                <textarea name="message"/>
-            <button type="submit">Send</button>
+    <div id="contact">
+        <form 
+        name="contact" 
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        >
+            <input name="name" placeholder="Your Name" type="text"/>
+            <button>Send</button>
         </form>
+    </div>
 )
