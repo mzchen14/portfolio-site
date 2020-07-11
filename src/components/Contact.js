8e715a51409f082870aa1291f6d87ef507aafe3f
Ming Chen <mzchen14@gmail.com>
2020-07-11 15:40:26 -0400

    Testing new form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,20 @@ export default () => (
         data-netlify-honeypot="bot-field"
         >
             <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />        
-            <input name="name" placeholder="Your Name" type="text"/>
+            <input type="hidden" name="form-name" value="contact" />
+
+            <div className="input">
+                <label htmlFor="name"> Your Name: </label>
+                <input name="name" placeholder="Your Name" type="text"/>
+            </div>
+            <div className="input">
+                <label htmlFor="email"> Your Email:</label>
+                <input name="email" placeholder="email@email.com" type="text"/>
+            </div>
+            <div className="input">
+                <label htmlFor="message"> Message </label>
+                <textarea name="message"/>
+            </div>
             <button>Send</button>
         </form>
     </div>
